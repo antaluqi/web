0dcfe420ca79715f8da60a06b20f18f137c824d9
antaluqi <33272769+antaluqi@users.noreply.github.com>
2017-11-24 16:22:32 +0800

    首次提交

---
+++ web/Comm.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Web.Script.Serialization;
+
+
+//-------------------------------------------------
+
+/*                  通用函数类                    */
+
+//-------------------------------------------------
+namespace Common
+{
+    class Comm
+    {
+        // 读取文本文件
+        public static string TxTRead(string filepath)
+        {
+            StreamReader sr = new StreamReader(filepath, Encoding.Default);
+            string allLine = "";
+            String line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                allLine = allLine + line.ToString() + "\n";
+            }
+            return allLine;
+        }
+        // 读取Json字符串（返回字符串字典）
+        public static dynamic JsonRead(string jsonStr)
+        {
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            dynamic modelDy = js.Deserialize<dynamic>(jsonStr); //反序列化
+            return modelDy;
+        }
+        // 读取网页文本（测试用，返回流）
+        public static StreamReader xmlRead(string path)
+        {
+            StreamReader reader = new StreamReader(path, Encoding.GetEncoding("utf-8"));
+            return reader;
+        }
+
+        public static void write2TxT(string path,string txt)
+        {
+            FileStream fs = new FileStream(path, FileMode.Create);
+            StreamWriter sw = new StreamWriter(fs);
+            //开始写入
+            sw.Write(txt);
+            //清空缓冲区
+            sw.Flush();
+            //关闭流
+            sw.Close();
+            fs.Close();
+        }
+    }
+
+}
---
+++ web/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using WWW16pu;
+using Common;
+using System.Text.RegularExpressions;
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String userStr=Comm.TxTRead(@"./用户.txt");
+            string username = Regex.Matches(userStr, @"(?<=用户名：)\d+")[0].Value;
+            string password = Regex.Matches(userStr, @"(?<=密码：)\d+")[0].Value;
+            //Console.WriteLine("用户名："+ username+ "\n密码:"+ password);
+
+            // www16pu pu = new www16pu("18658131087", "198221");
+            //www16pu pu = new www16pu("15988166652", "615919");
+           www16pu pu = new www16pu(username, password);
+            string logInfo="";
+            bool log = pu.Login(ref logInfo);
+           // bool log = pu.LoginFromCookie(ref logInfo);
+            Console.WriteLine(logInfo);
+
+            /*
+            MemberInfo memberInfo= pu.Member();
+            List<TzlbInfo> tzlbInfo= pu.MemberTzlb();
+            List<PdetailInfo> pdetailInfo= pu.Touzi();
+
+            Console.WriteLine("==============================================\n基本信息：>>>>>>>>>\n");
+              Console.WriteLine(memberInfo.ToString());
+            Console.WriteLine("==============================================\n已投资列表：>>>>>>>\n");
+               www16pu.InfoListView(tzlbInfo);
+            Console.WriteLine("==============================================\n可投标的：>>>>>>>>>\n");
+               www16pu.InfoListView(pdetailInfo);
+            */
+
+            //Console.WriteLine(pu.Buy(1000, "13178"));
+            pu.AutoBuy();
+
+            Console.WriteLine("按任意键退出");
+            Console.Read();
+        }
+    }
+
+}
+
